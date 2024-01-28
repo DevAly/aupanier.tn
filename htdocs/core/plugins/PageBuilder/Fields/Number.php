ff01dca86aeba2fed8fe694f65f5e2215c1b09f4
mohamedrayen <mohamedrayen.bouzaabia@esprit.tn>
2024-01-28 18:04:52 +0100

    fix

--- htdocs/core/plugins/PageBuilder/Fields/Number.php
+++ htdocs/core/plugins/PageBuilder/Fields/Number.php
@@ -20,7 +20,7 @@ class Number extends PageBuilderField
         $output = '';
         $output .= $this->field_before();
         $output .= $this->label();
-        $output .= '<input type="number" value="'.$this->value().'"   placeholder="'.$this->placeholder().'" name="'.$this->name().'"  class="'.$this->field_class().'"/>';
+        $output .= '<input type="number" value="'.$this->value().'"   placeholder="'.$this->placeholder().'" name="'.$this->name().'"  class="text-var-field '.$this->field_class().'"/>';
         $output .= $this->field_after();
 
         return $output;
